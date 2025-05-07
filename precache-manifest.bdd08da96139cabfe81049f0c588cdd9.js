d82c2686fea111413bcda18310dd559cede8d946
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2025-05-08 03:06:17 +0500

    Updates

---
+++ precache-manifest.bdd08da96139cabfe81049f0c588cdd9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f41aaf6fdafc34e19cf23a8130cc0e5f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6aa2c02e09c88a69a108",
+    "url": "./static/css/main.dff5d620.chunk.css"
+  },
+  {
+    "revision": "f5c06d92e0d37fc000da",
+    "url": "./static/js/2.c79dab43.chunk.js"
+  },
+  {
+    "revision": "db4976e2c3fc371be79e",
+    "url": "./static/js/3.1af95c37.chunk.js"
+  },
+  {
+    "revision": "6aa2c02e09c88a69a108",
+    "url": "./static/js/main.12e79d34.chunk.js"
+  },
+  {
+    "revision": "fb690272fee8e7af1280",
+    "url": "./static/js/runtime~main.71ad8aa4.js"
+  }
+]);
