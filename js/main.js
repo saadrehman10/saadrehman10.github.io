e385ccaae2e67d47de2a93abed1c68e232111264
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2025-05-23 05:13:10 +0500

    update

--- js/main.js
+++ js/main.js
@@ -37,7 +37,7 @@ $(document).ready( function() {
     if ($('.element').length) {
         $('.element').each(function () {
             $(this).typed({
-                strings: [$(this).data('text1'), $(this).data('text2'), $(this).data('text3'), $(this).data('text4')], 
+                strings: [$(this).data('text1'), $(this).data('text2'), $(this).data('text3'), $(this).data('text4'), $(this).data('text5')], 
                 loop: $(this).data('loop') ? $(this).data('loop') : false ,
                 backDelay: $(this).data('backdelay') ? $(this).data('backdelay') : 2000 ,                
                 typeSpeed: 10,
